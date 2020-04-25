28cb6c77194999495e5e056f61f769432af77fdf
Naveed Khan <naveed@gmail.com>
2020-04-24 21:28:44 -0700

    working server

--- server/app.py
+++ server/app.py
@@ -1,11 +1,24 @@
-
+import random
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return 'OK'
+
+
+@app.route('/quote')
+def quote():
+    return random.choice([
+        "A hungry man may only think of the food",
+        "Smart people have all the fun",
+        "Hard to beat someone who doesn't give up",
+        "Understanding yourself is the hardest thing in the world",
+        "You may only change the future",
+        "Choose habits over will power if you actually want to succeed"
+    ])
 
 
 if __name__ == '__main__':
